9f5a738e90b4c45a7f26f3b5bc0c0ce2df23eced
Keith Burnell <kburnell@skylinetechnologies.com>
2013-04-15 21:07:13 -0500

    Preparing for dotnetconf

--- Calculator_End/Calculator.Console/Program.cs
+++ Calculator_End/Calculator.Console/Program.cs
@@ -7,14 +7,6 @@ namespace Calculator.Console {
         private static decimal _value2;
         private static CalculatorService _calculatorService;
 
-        static void Main(string[] args) {
-            //_calculatorService = new CalculatorService();
-            System.Console.Write("Value 1: ");
-            _value1 = decimal.Parse(System.Console.ReadLine());
-            System.Console.Write("Value 2: ");
-            _value2 = decimal.Parse(System.Console.ReadLine());
-            System.Console.WriteLine("Calculated Value: " + _calculatorService.Add(_value1, _value2));
-            System.Console.ReadKey();
-        }
+        static void Main(string[] args) {}
     }
 }
--- Calculator_End/Calculator.Service/CalculatorService.cs
+++ Calculator_End/Calculator.Service/CalculatorService.cs
@@ -1,40 +1,13 @@
 ﻿using System;
 using Calculator.Service.Interfaces;
-using Calculator.Shared;
 
 namespace Calculator.Service {
 
     public class CalculatorService : ICalculatorService {
-        private IValidationService _validationService;
-
-        #region << Private Fields >>
-
-
-        #endregion
-
-        #region << Constructors >>
-
-        public CalculatorService() {
-            _validationService = DependencyResolver.GetConcreteInstanceOf<IValidationService>();
-        }
-
-        #endregion
-
-        #region << Public Properties >>
-
-        #endregion
-
-        #region << Public Methods >>
 
         public decimal Add(decimal value1, decimal value2) {
-            decimal calculatedResult = 0;
-            if (_validationService.ValidateForAdd(value1, value2)) {
-                
-            }
-            return calculatedResult;
+            throw new NotImplementedException("Add has not been implemented yet!");
         }
-
-        #endregion
 
     }
 }
