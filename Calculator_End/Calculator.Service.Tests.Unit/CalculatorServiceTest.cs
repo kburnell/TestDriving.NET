a2838815f56a29e05bc08f9b641fe4a9a8382857
Keith Burnell <kburnell@skylinetechnologies.com>
2013-04-16 08:11:03 -0500

    Progress on reworking presentation for dotNetConf

--- Calculator_End/Calculator.Service.Tests.Unit/CalculatorServiceTest.cs
+++ Calculator_End/Calculator.Service.Tests.Unit/CalculatorServiceTest.cs
@@ -1,10 +1,23 @@
-﻿
+﻿using FluentAssertions;
 using NUnit.Framework;
 
 namespace Calculator.Service.Tests.Unit {
 
     [TestFixture]
     public class CalculatorServiceTest {
-        
+
+        [Test]
+        public void Add_ShouldReturn_12_When_Passed_8_And_4()
+        {
+            //Arrange
+            decimal input1 = 8;
+            decimal input2 = 4;
+            CalculatorService classUnderTest = new CalculatorService();
+            //Act
+            var result = classUnderTest.Add(input1, input2);
+            //Assert
+            result.Should().Be(12);
+        }
+
     }
 }
--- Calculator_End/Calculator.Service/CalculatorService.cs
+++ Calculator_End/Calculator.Service/CalculatorService.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using Calculator.Service.Interfaces;
+﻿using Calculator.Service.Interfaces;
 
 namespace Calculator.Service {
 
     public class CalculatorService : ICalculatorService {
 
         public decimal Add(decimal value1, decimal value2) {
-            throw new NotImplementedException("Add has not been implemented yet!");
+           return value1 + value2;
         }
 
     }
